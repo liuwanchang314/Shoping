4e56f6ebfff2a5cfa7f350b213156a602c11a0f4
liuwanchang314 <285939136@qq.com>
2015-12-08 23:53:58 +0800

    确认支付界面

---
+++ src/com/customview/Mylistview.java
@@ -0,0 +1,34 @@
+package com.customview;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class Mylistview extends ListView {
+	
+	public Mylistview(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Mylistview(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Mylistview(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+	 @Override
+     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+             int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                             MeasureSpec.AT_MOST);
+
+             super.onMeasure(widthMeasureSpec, expandSpec);
+     }
+	
+	
+	
+
+}
