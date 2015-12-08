9416f47109c19f07a794cbbb9ae27c77a8cbd034
liuwanchang314 <285939136@qq.com>
2015-12-08 18:00:15 +0800

    测试第三次

--- src/com/activity/MainActivity.java
+++ src/com/activity/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends TabActivity {
 	//协作开发测试第一次
 	
 	RadioButton t1, t2, t3, t4;
+	
 	private static TabHost tabHost;
 	private static Context context;
 	// private TextView main_tab_new_message;
