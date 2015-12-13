0a213f5abf218c32e8a68fa64ba2387ec739bdc4
liuwanchang314 <285939136@qq.com>
2015-12-13 17:59:06 +0800

    全部商品以及公司公告解析类

--- src/com/jsonParser/AllProductDataJson.java
+++ src/com/jsonParser/AllProductDataJson.java
@@ -1,93 +1,101 @@
 package com.jsonParser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.bean.AllProductBean;
+
 /**
  * {
     "api": "APISUCCESS",
-    "data": {
-        "884": {
-            "class_info": "",
-            "class_name": "¶à¼þÌ× ",
-            "class_column": "4",
-            "class_sort": "2",
-            "class_goods": []
+    "data": [
+        {
+            "goods_id": "4400",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20151015015628876.jpg_250_250.jpg",
+            "goods_price": "0.01"
         },
-        "885": {
-            "class_info": "",
-            "class_name": "ËÄ¼þÌ× ",
-            "class_column": "4",
-            "class_sort": "1",
-            "class_goods": []
+        {
+            "goods_id": "27019",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20151015015802157.jpg_250_250.jpg",
+            "goods_price": "10.00"
         },
-        "28316": {
-            "class_info": "<img src=\"http://www.91jf.com/attachment/editor/image/20151127/20151127101318_13539.jpg\" alt=\"\" />",
-            "class_name": "",
-            "class_column": "4",
-            "class_sort": "99",
-            "class_goods": [
-                {
-                    "goods_id": "4400",
-                    "goods_price": "0.01",
-                    "goods_name": "½ö¹©²âÊÔÇëÎð¹ºÂò",
-                    "goods_image": "/attachment/goods/428/20151015015628876.jpg_250_250.jpg"
-                },
-                {
-                    "goods_id": "8459",
-                    "goods_price": "10.00",
-                    "goods_name": "½ö¹©²âÊÔÇëÎð¹ºÂò",
-                    "goods_image": "/attachment/goods/428/20150813020848288.jpg_250_250.jpg"
-                },
-                {
-                    "goods_id": "21129",
-                    "goods_price": "12.00",
-                    "goods_name": "½ö¹©²âÊÔÇëÎð¹ºÂò",
-                    "goods_image": "/attachment/goods/428/20150924113134773.jpg_250_250.jpg"
-                },
-                {
-                    "goods_id": "21130",
-                    "goods_price": "120.00",
-                    "goods_name": "91¼Ò·ÄÍøÃ«Ìº",
-                    "goods_image": "/attachment/goods/428/20151015015908781.jpg_250_250.jpg"
-                }
-            ]
+        {
+            "goods_id": "8459",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20150813020848288.jpg_250_250.jpg",
+            "goods_price": "10.00"
         },
-        "28318": {
-            "class_info": "<img src=\"http://www.91jf.com/attachment/editor/image/20151208/20151208113435_67477.jpg\" alt=\"\" />",
-            "class_name": "",
-            "class_column": "4",
-            "class_sort": "99",
-            "class_goods": [
-                {
-                    "goods_id": "21130",
-                    "goods_price": "120.00",
-                    "goods_name": "91¼Ò·ÄÍøÃ«Ìº",
-                    "goods_image": "/attachment/goods/428/20151015015908781.jpg_250_250.jpg"
-                }
-            ]
+        {
+            "goods_id": "21129",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20150924113134773.jpg_250_250.jpg",
+            "goods_price": "12.00"
         },
-        "28320": {
-            "class_info": "<img src=\"http://www.91jf.com/attachment/editor/image/20151208/20151208113446_57597.jpg\" alt=\"\" />",
-            "class_name": "",
-            "class_column": "4",
-            "class_sort": "99",
-            "class_goods": [
-                {
-                    "goods_id": "4400",
-                    "goods_price": "0.01",
-                    "goods_name": "½ö¹©²âÊÔÇëÎð¹ºÂò",
-                    "goods_image": "/attachment/goods/428/20151015015628876.jpg_250_250.jpg"
-                },
-                {
-                    "goods_id": "8459",
-                    "goods_price": "10.00",
-                    "goods_name": "½ö¹©²âÊÔÇëÎð¹ºÂò",
-                    "goods_image": "/attachment/goods/428/20150813020848288.jpg_250_250.jpg"
-                }
-            ]
+        {
+            "goods_id": "21130",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20151015015908781.jpg_250_250.jpg",
+            "goods_price": "120.00"
+        },
+        {
+            "goods_id": "29318",
+            "goods_name": null,
+            "goods_image": "/attachment/banner/201510/20151024021124798.jpg",
+            "goods_price": "699.00"
+        },
+        {
+            "goods_id": "18604",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20151015015741174.jpg_250_250.jpg",
+            "goods_price": "16.00"
+        },
+        {
+            "goods_id": "27020",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20150813022154705.jpg_250_250.jpg",
+            "goods_price": "0.01"
+        },
+        {
+            "goods_id": "12238",
+            "goods_name": null,
+            "goods_image": "/attachment/goods/428/20150813022205559.jpg_250_250.jpg",
+            "goods_price": "20.00"
         }
-    },
+    ],
     "status": 1
 }
  * */
 public class AllProductDataJson {
+	
+	public static List<AllProductBean> GetProductData(String jsonstring){
+		List<AllProductBean> list=new ArrayList<AllProductBean>();
+		try {
+			JSONObject obj=new JSONObject(jsonstring);
+			JSONArray array=obj.getJSONArray("data");
+			int count=array.length();
+			for(int i=0;i<count;i++){
+				//Éú³ÉÊµÌåÀà¶ÔÏó
+				AllProductBean bean=new AllProductBean();
+				JSONObject objs=array.getJSONObject(i);
+				bean.setId(objs.getString("goods_id"));
+				bean.setImage(objs.getString("goods_image"));
+				bean.setName(objs.getString("goods_name"));
+				bean.setPrice(objs.getString("goods_price"));
+				list.add(bean);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list;
+		
+	}
 
 }
