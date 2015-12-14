1957ca1223d95079746dd237a8d0b89c3121a7f6
liuwanchang314 <285939136@qq.com>
2015-12-14 21:25:46 +0800

    修改了actionbar的显示

--- src/com/activity/BuyActivity.java
+++ src/com/activity/BuyActivity.java
@@ -27,7 +27,6 @@ public class BuyActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
 		setContentView(R.layout.activity_buy);
 		
 	}
